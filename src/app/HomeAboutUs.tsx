c8bd380eb8e9d82dfd5f4e2eedbd1754b86943c7
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-11-23 11:57:27 +0700

    fix responsive problem on the about us section

--- src/app/HomeAboutUs.tsx
+++ src/app/HomeAboutUs.tsx
@@ -7,8 +7,8 @@ const HomeAboutUs = () => {
   const button_bg = "#a3cfba";
   const button_bg_hover = "#8FC0A9";
   return (
-    <section className="relative w-full h-max lg:h-screen p-[3.2rem] py-[6.4rem] lg:p-[6.4rem] flex flex-col lg:flex-row items-start lg:items-center justify-center lg:justify-between lg:gap-[6.4rem]">
-      <div className="w-fit flex flex-col justify-center gap-[3.2rem]">
+    <section className="relative max-w-screen w-full h-max lg:h-screen p-[3.2rem] py-[6.4rem] lg:p-[6.4rem] flex flex-col lg:flex-row items-start lg:items-center justify-center lg:justify-between lg:gap-[3.2rem]">
+      <div className="w-full flex flex-col justify-center gap-[3.2rem]">
         
         <LineAndText text="KNOW ABOUT US" />
 
@@ -17,11 +17,11 @@ const HomeAboutUs = () => {
         </div>
 
         <div className="w-full h-fit flex flex-col gap-[3.2rem] lg:gap-[6.4rem] lg:pl-[6.4rem] justify-center">
-          <div className="w-fit">
-            <h3 data-text-reveal className="merri-bold bold text-[3.2rem] lg:text-[4.8rem] w-full lg:w-[58rem] lg:mb-0 mb-3">
+          <div className="w-full">
+            <h3 data-text-reveal className="merri-bold bold text-[3.2rem] lg:text-[4.8rem] w-full lg:mb-0 mb-3">
               We provide a place for children to develop their talents.
             </h3>
-            <p data-text-reveal className="jakarta-regular text-[1.8rem] w-full lg:w-[58rem]">
+            <p data-text-reveal className="jakarta-regular text-[1.8rem] w-full ">
               We educate the next generation as followers of Christ, spreading
               light and hope, so that we can shape children into instruments fo
               r the glory of God.
@@ -41,7 +41,7 @@ const HomeAboutUs = () => {
         </div>
       </div>
 
-      <div className="bg-red-200 relative hidden lg:block min-w-[43.5rem] h-[70vh] rounded-lg">
+      <div className="relative hidden lg:block min-w-[43.5rem] h-[70vh] rounded-lg">
         <Image
           src={"/images/test.jpg"}
           alt="logo"
