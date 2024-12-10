bc48e84915614c7193785aa70612c8a66f8229dc
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-12-10 16:28:29 +0700

    test change icon 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,12 @@ import "../styles/globals.css";
 import GrammarlyHandler from "@/components/GrammarlyHandler";
 
 export const metadata: Metadata = {
-  title: "Indonesia Hope House Foundation",
-  description: "Testing of Hope House Foundation",
+  title:{
+    default: "Indonesia Hope House Foundation",
+    template: "%s - Indonesia Hope House Foundation"
+  } ,
+  description: "Indonesia Hope House Foundation is an organization dedicated to providing hope and a meaningful life to the young generation. Based on the teachings of God's love, we are committed to preparing them to become the light and salt of the world, and to become fishers of men according to the divine calling.",
+  icons: 'favicon.ico'
 };
 
 interface RootLayoutProps {
