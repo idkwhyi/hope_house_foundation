c9499d4949d8db60d326e5bbc008dc4433d96234
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-12-10 17:57:56 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,54 +1,57 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+
+  const MAIN_URL = "https://hope-house-foundation.vercel.app/"
+
   return [
     {
-      url: 'https://hope-house-foundation.vercel.app/',
+      url: `${MAIN_URL}`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://hope-house-foundation.vercel.app/about',
+      url: `${MAIN_URL}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'http://localhost:3000/about/harapan-bagi-bangsa',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'http://localhost:3000/about/little-house',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'http://localhost:3000/about/character-building-school',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'http://localhost:3000/about/shelter-home',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'http://localhost:3000/contact',
+      url: `${MAIN_URL}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'http://localhost:3000/donation',
+      url: `${MAIN_URL}/donation`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
+    },
+    {
+      url: `${MAIN_URL}/about/harapan-bagi-bangsa`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${MAIN_URL}/about/little-house`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${MAIN_URL}/about/character-building-school`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${MAIN_URL}/about/shelter-home`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
   ]
 }
