05090c2f65c5953224ae079045ad0b2300888687
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-12-10 18:03:59 +0700

    update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,11 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "",
+        userAgent: "*",
         allow: "/",
         disallow: []
       }
     ],
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+    sitemap: `https://hope-house-foundation.vercel.app/sitemap.xml`
   }
 }
